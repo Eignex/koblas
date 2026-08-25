f368791324784f91dce1d2bc06769f13fa9dba87
Rasmus Ros <rasmus@eignex.com>
2026-08-25 20:18:30 +0200

    feat: add a basis solver seam for the simplex

--- koblas/build.gradle.kts
+++ koblas/build.gradle.kts
@@ -91,6 +91,7 @@ dokka {
             "src/commonMain/kotlin/com/eignex/koblas/package.md",
             "src/commonMain/kotlin/com/eignex/koblas/dense/package.md",
             "src/commonMain/kotlin/com/eignex/koblas/sparse/package.md",
+            "src/commonMain/kotlin/com/eignex/koblas/sparse/basis/package.md",
         )
     }
 }
